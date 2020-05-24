929dd5c69f146ed907a2f6fe4a98786d7a0d41cf
jadielmwangi <jedielmwangi@gmail.com>
2020-05-24 23:08:25 +0300

    adds  category  model which gives categories and description of images

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,21 @@ import datetime as dt
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.update_category()
+
 
 
 class Image(models.Model):
