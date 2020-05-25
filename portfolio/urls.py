8a66f75842b912fe84433e23890ff584774dc6d6
jadielmwangi <jedielmwangi@gmail.com>
2020-05-25 09:47:45 +0300

    adds   ##register the MEDIA_ROOT route inorder to serve uploaded images on the development server

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -12,5 +12,7 @@ urlpatterns=[
 
     url(r'^search/', views.search_results, name='search_results'),
 ]
+
+##register the MEDIA_ROOT route inorder to serve uploaded images on the development server
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
