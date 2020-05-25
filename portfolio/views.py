25d8a4060b98ac99ebec0af3bde5d156b7cefe1b
jadielmwangi <jedielmwangi@gmail.com>
2020-05-25 09:36:24 +0300

    adds   create the view function that will handle the logic for displaying the search results

--- portfolio/views.py
+++ portfolio/views.py
@@ -27,13 +27,13 @@ def contact(request):
 
 
 
-
+## view function that will handle the logic for displaying the search results
 def search_results(request):
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
         searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
-
+##render a HTML template and pass in the list of items found and the search_term.
         return render(request, 'all-folios/search.html',{"message":message,"images": searched_images})
 
     else:
